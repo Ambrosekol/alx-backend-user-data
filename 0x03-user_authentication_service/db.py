172b6bafb5c2819bfb679ed79e34c2b6f0a670fa
ambrose <kolus7381@gmail.com>
2024-02-19 17:31:09 +0100

    Update user added

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add new user to db"""
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            user = None
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds a user using arbituary kwargs"""
+        keys = []
+        for key, val in kwargs.items():
+            if hasattr(User, key):
+                keys.append(key)
+            else:
+                raise InvalidRequestError()
+        for key in keys:
+            result = self._session.query(User)\
+                .filter(User.__getattribute__(User,
+                                              key) == kwargs.get(key)).first()
+            if result is None:
+                raise NoResultFound()
+            return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user"""
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        prams = {}
+        for key, val in kwargs.items():
+            if hasattr(User, key):
+                prams[key] = val
+            else:
+                raise ValueError
+        self._session.query(User)\
+            .filter(User.id == user.id).update(prams,
+                                               synchronize_session=False)
+        self._session.commit()
